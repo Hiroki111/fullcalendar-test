c9110d92c858c2489a76927a603b60b06fe882fa
Hiroki 111 <mitsuke1116@gmail.com>
2019-06-18 08:16:58 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import './App.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return (<FullCalendar
+    defaultView="timeGridWeek"
+    plugins={[timeGridPlugin]}
+    timeZone={'UTC'}
+    events={[
+      { title: "Ceremony 1", start: new Date("2019-06-17 10:00"), end: new Date("2019-06-17 11:30") },
+      { title: "Ceremony 2", start: new Date("2019-06-17 13:00"), end: new Date("2019-06-17 14:00") },
+      { title: "Ceremony 3", start: new Date("2019-06-18 13:00"), end: new Date("2019-06-18 14:00") },
+      { title: "Ceremony 4", start: new Date("2019-06-18 13:30"), end: new Date("2019-06-18 14:00") },
+      { title: "Ceremony 5", start: new Date("2019-06-19 10:00"), end: new Date("2019-06-19 11:30") },
+    ]}
+    header={{
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek'
+    }}
+    editable={true}
+  />);
 }
 
 export default App;
